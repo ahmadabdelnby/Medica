66144c2ac408db80fecbe854c8ff2740bc1001a7
Mustafa Mahmoud <moustafa.mahnoud2024@gmail.com>
2024-06-13 17:48:51 +0300

    more

--- src/app/Modules/clinics/clinics.module.ts
+++ src/app/Modules/clinics/clinics.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClinicloginComponent } from './cliniclogin/cliniclogin.component';
 import { UrprofileComponent } from './urprofile/urprofile.component';
-import { ReportComponent } from './report/report.component';
 import { DoctorReservationComponent } from './doctor-reservation/doctor-reservation.component';
 
 @NgModule({
   declarations: [
     ClinicloginComponent,
     UrprofileComponent,
-    ReportComponent,
     DoctorReservationComponent,
   ],
   imports: [CommonModule],
